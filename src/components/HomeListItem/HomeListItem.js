a37f6e821351ee601be5e7a432eaa344ad7efa3b
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-12-11 22:54:03 -0600

    added comments to homelistiem component

--- src/components/HomeListItem/HomeListItem.js
+++ src/components/HomeListItem/HomeListItem.js
@@ -10,7 +10,9 @@ class HomeListItem extends Component {
 	sendToDeets = () => {
 		console.log("movie id is:", this.props.movie.id);
 		this.props.dispatch({
+			//sends to individual movie reducer
 			type: "FETCH_IND_MOVIE",
+			//sends the individual movie url
 			url: `/api/movie/${this.props.movie.id}`,
 		});
 		this.props.history.push("/Details");
